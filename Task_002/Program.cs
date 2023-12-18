d42216a2f4120d0ba70ec534b79ec4053d545e6f
nataliya <vasilenkonatalia02@gmail.com>
2023-12-18 20:49:37 +0300

    Добавили решение Task_002

---
+++ Task_002/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Задача 2.
+
+
+
+ var array = new int[4, 4];
+           
+            for(int i = 0; i < array.GetLength(1); i++)
+            {
+                var tmp = array[3, i];
+                array[3, i] = array[0, i];
+                array[0, i] = tmp;
+            }
+            for(int i = 0; i < array.GetLength(0); i++)
+            {
+                for(int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+         
